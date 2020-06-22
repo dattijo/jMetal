f761b2413e22210530708905a990893d4840e24c
PhDLab <PhDLab@DESKTOP-0QKGPRD>
2020-06-22 11:22:42 +0100

    Re-Constructive Algorithm

--- jmetal-example/src/main/java/org/uma/jmetal/example/multiobjective/nsgaii/jmetal5version/NSGAIIETPRunner.java
+++ jmetal-example/src/main/java/org/uma/jmetal/example/multiobjective/nsgaii/jmetal5version/NSGAIIETPRunner.java
@@ -42,118 +42,129 @@ import org.uma.jmetal.operator.mutation.impl.TimeslotSwapMutation;
 //import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
 
 /**
- * Class for configuring and running the NSGA-II algorithm to solve the bi-objective ETP
+ * Class for configuring and running the NSGA-II algorithm to solve the
+ * bi-objective ETP
  *
  * @author Ahmad A. Datti <aadatti.cs@buk.edu.ng>
  */
 public class NSGAIIETPRunner extends AbstractAlgorithmRunner {
-  /**
-   * @param args Command line arguments.
-   * @throws java.io.IOException
-   * @throws SecurityException
-   */
-  public static void main(String[] args) throws JMetalException, IOException {
-    JMetalRandom.getInstance().setSeed(100L);
 
-    IntegerMatrixProblem problem;
-    Algorithm<List<IntegerMatrixSolution<Integer>>> algorithm;
-    CrossoverOperator<IntegerMatrixSolution<Integer>> crossover;
-    MutationOperator<IntegerMatrixSolution<Integer>> mutation;
-    MutationOperator<IntegerMatrixSolution<Integer>> localSearchMutation;
-    LocalSearchOperator<IntegerMatrixSolution<Integer>> localSearch;
-    SelectionOperator<List<IntegerMatrixSolution<Integer>>, IntegerMatrixSolution<Integer>> selection;
-    RankingAndCrowdingDistanceComparator<IntegerMatrixSolution<Integer>> comparator;
-    
+    /**
+     * @param args Command line arguments.
+     * @throws java.io.IOException
+     * @throws SecurityException
+     */
+    public static void main(String[] args) throws JMetalException, IOException {
+        JMetalRandom.getInstance().setSeed(100L);
+
+        IntegerMatrixProblem problem;
+        Algorithm<List<IntegerMatrixSolution<Integer>>> algorithm;
+        CrossoverOperator<IntegerMatrixSolution<Integer>> crossover;
+        MutationOperator<IntegerMatrixSolution<Integer>> mutation;
+        MutationOperator<IntegerMatrixSolution<Integer>> localSearchMutation;
+        LocalSearchOperator<IntegerMatrixSolution<Integer>> localSearch;
+        SelectionOperator<List<IntegerMatrixSolution<Integer>>, IntegerMatrixSolution<Integer>> selection;
+        RankingAndCrowdingDistanceComparator<IntegerMatrixSolution<Integer>> comparator;
+
 //    problem = new MultiobjectiveETP("C:/Users/PhDLab/Documents/NetBeansProjects/examTimetableDataReader/exam_comp_set44.exam",
 //            "C:/Users/PhDLab/Documents/NetBeansProjects/jMetal/jmetal-example/examDifficulty.fcl",
 //            "C:/Users/PhDLab/Documents/NetBeansProjects/jMetal/jmetal-example/examDifficultyData");
+        String mainPath = "C:/Users/PhDLab/Documents/NetBeansProjects/jMetal/jmetal-example";
+        String examDatasetFile = "/dataset/modifiedITC2007/exam_comp_set44.exam";
+        String examDifficultyData = "/fuzzyData/exam_comp_set_any.diff";
+        String fuzzyLogicFile = "/fuzzyData/examDifficulty.fcl";
 
-    String mainPath = "C:/Users/PhDLab/Documents/NetBeansProjects/jMetal/jmetal-example";
-    String examDatasetFile = "/dataset/modifiedITC2007/exam_comp_set44.exam";
-    String fuzzyLogicFile = "/fuzzyData/examDifficulty.fcl";
-    String examDifficultyData = "/fuzzyData/examDifficultyData";      
+        problem = new MultiobjectiveETP(mainPath + examDatasetFile, mainPath + fuzzyLogicFile, mainPath + examDifficultyData);
 
-    problem = new MultiobjectiveETP(mainPath+examDatasetFile, mainPath+fuzzyLogicFile, mainPath+examDifficultyData);
-    
-    ArrayList largestExams = problem.getLargestExams();
-    int [][] conflictMatrix =  problem.getConflictMatrix(); 
-    int [] roomCapacities = problem.getRoomCapacities();
-    int [] examEnrollments = problem.getExamEnrollments();        
-    int numberOfTimeslots = problem.getNumberOfTimeslots();    
-    int improvementRounds = 0;
-    int examsCount=2;
-        
-    boolean earlierTimeslot=true; 
-    boolean changeRoom = true;
-    boolean kempeChain=true;
-    
-    double mutationProbability = 0.9; 
-    crossover = new NullCrossover();       
-    mutation = new TimeslotShuffleMutation(mutationProbability, numberOfTimeslots);
-    localSearchMutation = new NullMutation();
-        
-    int rand = (int)(9*Math.random());
-        switch(rand){
+        ArrayList largestExams = problem.getLargestExams();
+        int[][] conflictMatrix = problem.getConflictMatrix();
+        int[] roomCapacities = problem.getRoomCapacities();
+        int[] examEnrollments = problem.getExamEnrollments();
+        int numberOfTimeslots = problem.getNumberOfTimeslots();
+        int improvementRounds = 0;
+        int examsCount = 2;
+
+        boolean earlierTimeslot = true;
+        boolean changeRoom = true;
+        boolean kempeChain = true;
+
+        double mutationProbability = 0.9;
+        crossover = new NullCrossover();
+        mutation = new TimeslotShuffleMutation(mutationProbability, numberOfTimeslots);
+        localSearchMutation = new NullMutation();
+
+        int rand = (int) (9 * Math.random());
+        switch (6) {
             //Move 1 or more randomly selected exam(s) to random feasible timeslot(s)
-            case 0: localSearchMutation = new ExamMoveMutation(mutationProbability, numberOfTimeslots, examsCount);
-                System.out.println("Move "+examsCount+" randomly selected exam(s) to random feasible timeslot(s)");
+            case 0:
+                localSearchMutation = new ExamMoveMutation(mutationProbability, numberOfTimeslots, examsCount);
+                System.out.println("Move " + examsCount + " randomly selected exam(s) to random feasible timeslot(s)");
                 break;
             //Move 1 randomly selected exam  to random feasible timeslot and room
-            case 1: localSearchMutation = new ExamMoveMutation(mutationProbability, numberOfTimeslots, changeRoom, roomCapacities, examEnrollments);
+            case 1:
+                localSearchMutation = new ExamMoveMutation(mutationProbability, numberOfTimeslots, changeRoom, roomCapacities, examEnrollments);
                 System.out.println("Move 1 randomly selected exam  to random feasible timeslot and room");
-                break;            
+                break;
             //Move 1 large exam to random ealier feasible timeslot
-            case 2: localSearchMutation = new ExamMoveMutation(mutationProbability, numberOfTimeslots, largestExams, earlierTimeslot);
+            case 2:
+                localSearchMutation = new ExamMoveMutation(mutationProbability, numberOfTimeslots, largestExams, earlierTimeslot);
                 System.out.println("Move 1 large exam to random ealier feasible timeslot");
                 break;
-            
+
             //Move all exams in a randomly selected timeslot to another feasible timeslot            
-            case 3: localSearchMutation = new TimeslotMoveMutation(mutationProbability, numberOfTimeslots);
+            case 3:
+                localSearchMutation = new TimeslotMoveMutation(mutationProbability, numberOfTimeslots);
                 System.out.println("Move all exams in a randomly selected timeslot to another feasible timeslot");
                 break;
             //Swap all exams in 2 randomly selected timeslots
-            case 4: localSearchMutation = new TimeslotSwapMutation(mutationProbability, numberOfTimeslots);
+            case 4:
+                localSearchMutation = new TimeslotSwapMutation(mutationProbability, numberOfTimeslots);
                 System.out.println("Swap all exams in 2 randomly selected timeslots");
                 break;
             //Shuffle all timeslots
-            case 5: localSearchMutation = new TimeslotShuffleMutation(mutationProbability, numberOfTimeslots);
+            case 5:
+                localSearchMutation = new TimeslotShuffleMutation(mutationProbability, numberOfTimeslots);
                 System.out.println("Shuffle all timeslots");
                 break;
-            
+
             //Move 1 randomly selected exam to a another randomly selected timeslot using the KempeChain Intercange            
-            case 6: localSearchMutation = new Kinterchange(mutationProbability, conflictMatrix, problem, kempeChain);
+            case 6:
+                localSearchMutation = new Kinterchange(mutationProbability, conflictMatrix, problem, kempeChain);
                 System.out.println("Move 1 randomly selected exam to a another randomly selected timeslot using the KempeChain Intercange");
                 break;
             //Swap timeslots of 2 randomly selected exams if feasible
-            case 7: localSearchMutation = new Kinterchange(mutationProbability, conflictMatrix, problem);
+            case 7:
+                localSearchMutation = new Kinterchange(mutationProbability, conflictMatrix, problem);
                 System.out.println("Swap timeslots of 2 randomly selected exams if feasible");
                 break;
-            
+
             //Move a randomly selected exam to a new feasible room within the same timeslot            
-            case 8: localSearchMutation = new RoomMoveMutation(mutationProbability, roomCapacities, examEnrollments); 
+            case 8:
+                localSearchMutation = new RoomMoveMutation(mutationProbability, roomCapacities, examEnrollments);
                 System.out.println("Move a randomly selected exam to a new feasible room within the same timeslot");
                 break;
             //Swap the rooms of 2 randomly selected exam if feasible.
-            case 9: localSearchMutation = new RoomSwapMutation(mutationProbability, roomCapacities, examEnrollments);
+            case 9:
+                localSearchMutation = new RoomSwapMutation(mutationProbability, roomCapacities, examEnrollments);
                 System.out.println("Swap the rooms of 2 randomly selected exam if feasible");
                 break;
         }
-        
-    selection =
-        new BinaryTournamentSelection<IntegerMatrixSolution<Integer>>(
-            new RankingAndCrowdingDistanceComparator<IntegerMatrixSolution<Integer>>());
 
-    int populationSize = 100;   //int populationSize = 100;
-    int offspringPopulationSize = populationSize;
-    
-    Termination termination  = new TerminationByEvaluations(1000);
-    comparator = new RankingAndCrowdingDistanceComparator<IntegerMatrixSolution<Integer>>();
-    localSearch = new GreatDelugeAlgorithm(improvementRounds, localSearchMutation, comparator, problem);
-    algorithm = 
-        new NSGAII<>(
-                problem, populationSize, offspringPopulationSize, crossover, mutation,
-                termination, localSearch)
-            .setEvaluation(new MultithreadedEvaluation<>(8));
+        selection
+                = new BinaryTournamentSelection<IntegerMatrixSolution<Integer>>(
+                        new RankingAndCrowdingDistanceComparator<IntegerMatrixSolution<Integer>>());
+
+        int populationSize = 20;   //int populationSize = 100;
+        int offspringPopulationSize = populationSize;
+
+        Termination termination = new TerminationByEvaluations(20);
+        comparator = new RankingAndCrowdingDistanceComparator<IntegerMatrixSolution<Integer>>();
+        localSearch = new GreatDelugeAlgorithm(improvementRounds, localSearchMutation, comparator, problem);
+        algorithm
+                = new NSGAII<>(
+                        problem, populationSize, offspringPopulationSize, crossover, mutation,
+                        termination, localSearch)
+                        .setEvaluation(new MultithreadedEvaluation<>(8));
 
 //    SERIAL    
 //    algorithm =
@@ -162,20 +173,19 @@ public class NSGAIIETPRunner extends AbstractAlgorithmRunner {
 //            .setSelectionOperator(selection)
 //            .setMaxEvaluations(1000) //.setMaxEvaluations(10000)
 //            .build();
+        AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm).execute();
 
-    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm).execute();
+        List<IntegerMatrixSolution<Integer>> population = algorithm.getResult();
+        long computingTime = algorithmRunner.getComputingTime();
 
-    List<IntegerMatrixSolution<Integer>> population = algorithm.getResult();
-    long computingTime = algorithmRunner.getComputingTime();
+        new SolutionListOutput(population)
+                .setVarFileOutputContext(new DefaultFileOutputContext("VAR.tsv"))
+                .setFunFileOutputContext(new DefaultFileOutputContext("FUN.tsv"))
+                .print();
 
-    new SolutionListOutput(population)
-        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.tsv"))
-        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.tsv"))
-        .print();
-
-    JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
-    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
-    JMetalLogger.logger.info("Objectives values have been written to file FUN.tsv");
-    JMetalLogger.logger.info("Variables values have been written to file VAR.tsv");
-  }
+        JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+        JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+        JMetalLogger.logger.info("Objectives values have been written to file FUN.tsv");
+        JMetalLogger.logger.info("Variables values have been written to file VAR.tsv");
+    }
 }
